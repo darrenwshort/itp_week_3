f39fe03ce6b464fd7da9f2028ee5ea7602f95bca
darrenwshort <w1765868@apps.losrios.edu>
2021-09-11 08:13:17 -0700

    complete RnM exercise

--- day_3/exercise.py
+++ day_3/exercise.py
@@ -1,6 +1,7 @@
+import os
 import requests
 import json
-import pprint
+import openpyxl
 
 
 response = requests.get("https://rickandmortyapi.com/api/character")
@@ -8,134 +9,51 @@ response = requests.get("https://rickandmortyapi.com/api/character")
 
 # json.loads() - takes text string and converts it to dict (clean_data)
 clean_data = json.loads(response.text)
-# print(response.text)
-# print(type(response.text))
 
-# print type of clean_data to confirm it is dict
-#print(type(clean_data))
+# grab only results list from clean_data dict, to work with
+results = clean_data['results']
 
-# test print and get "info"
-#print(clean_data)
 ############## Rick & Morty API exercise ################
-#pprint.pprint(clean_data)
-## go through results for each row in an excel spreadsheet
-## grab the name, species, gender, location name
+# go through results for each row in an excel spreadsheet
+# grab the name, species, gender, location name
 
-print()
-for key in clean_data.keys():
-    if key == 'results':
-        for result_dict in clean_data[key]:
-            #print(type(result_dict)) #dict
-            print(f"Name: {result_dict['name']}")
-            print(f"Species: {result_dict['species']}")
-            print(f"Gender: {result_dict['gender']}")
-            print(f"Location: {result_dict['location']['name']}\n")    
+# create workbook
+wb = openpyxl.Workbook()
 
-## [results list]
+# name of output file/workbook
+output_file = 'RickNMortyOutput.xlsx'
 
-# darren@iMac itp_week_3 % /usr/local/bin/python3 /Users/darren/Desktop/itp_week_3/day_3/exercise.py
+# grab sheet; randomly grabbing active sheet
+sheet = wb.active
 
-# Name: Rick Sanchez
-# Species: Human
-# Gender: Male
-# Location: Earth (Replacement Dimension)
-
-# Name: Morty Smith
-# Species: Human
-# Gender: Male
-# Location: Earth (Replacement Dimension)
-
-# Name: Summer Smith
-# Species: Human
-# Gender: Female
-# Location: Earth (Replacement Dimension)
-
-# Name: Beth Smith
-# Species: Human
-# Gender: Female
-# Location: Earth (Replacement Dimension)
-
-# Name: Jerry Smith
-# Species: Human
-# Gender: Male
-# Location: Earth (Replacement Dimension)
-
-# Name: Abadango Cluster Princess
-# Species: Alien
-# Gender: Female
-# Location: Abadango
-
-# Name: Abradolf Lincler
-# Species: Human
-# Gender: Male
-# Location: Testicle Monster Dimension
-
-# Name: Adjudicator Rick
-# Species: Human
-# Gender: Male
-# Location: Citadel of Ricks
-
-# Name: Agency Director
-# Species: Human
-# Gender: Male
-# Location: Earth (Replacement Dimension)
-
-# Name: Alan Rails
-# Species: Human
-# Gender: Male
-# Location: Worldender's lair
-
-# Name: Albert Einstein
-# Species: Human
-# Gender: Male
-# Location: Earth (Replacement Dimension)
-
-# Name: Alexander
-# Species: Human
-# Gender: Male
-# Location: Anatomy Park
-
-# Name: Alien Googah
-# Species: Alien
-# Gender: unknown
-# Location: Earth (Replacement Dimension)
-
-# Name: Alien Morty
-# Species: Alien
-# Gender: Male
-# Location: Citadel of Ricks
-
-# Name: Alien Rick
-# Species: Alien
-# Gender: Male
-# Location: Citadel of Ricks
-
-# Name: Amish Cyborg
-# Species: Alien
-# Gender: Male
-# Location: Earth (Replacement Dimension)
-
-# Name: Annie
-# Species: Human
-# Gender: Female
-# Location: Anatomy Park
-
-# Name: Antenna Morty
-# Species: Human
-# Gender: Male
-# Location: Citadel of Ricks
-
-# Name: Antenna Rick
-# Species: Human
-# Gender: Male
-# Location: unknown
-
-# Name: Ants in my Eyes Johnson
-# Species: Human
-# Gender: Male
-# Location: Interdimensional Cable
+# create headers/column titles on 1st row of sheet
+sheet['A1'] = "Name"
+sheet['B1'] = "Species"
+sheet['C1'] = "Gender"
+sheet['D1'] = "Location"
 
 
+################## FOR SELF ONLY ######################
+# clear terminal for better readabilty of output/errors
+# when running script
+os.system('clear')  
+#######################################################
 
+# initialize counter to 2 (2nd row; "1st row" after header row)
+# loop through each Rick and Morty character, populating 1 row
+# per show character.
+# increment 'counter' as each character is processed.
+counter = 2
+for character in results:
+    sheet['A' + str(counter)] = character['name']
+    sheet['B' + str(counter)] = character['species']
+    sheet['C' + str(counter)] = character['gender']
+    sheet['D' + str(counter)] = character['location']['name']
+    counter += 1
 
+# delete existing output file before save
+os.remove('./' + output_file)
 
+# save workbook to file.
+wb.save(filename = './' + output_file)
+        
